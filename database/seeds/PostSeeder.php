af75f411656d9242786a6f7ec48dbb6b5cf2d4c0
Ümit UZ <umituz998@gmail.com>
2020-05-24 02:55:34 +0300

    PostSeeder

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+/**
+ * Class PostSeeder
+ */
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        DB::table('posts')->truncate();
+
+        DB::table('posts')->insert([
+            ['user_id' => 1, 'body' => 'First very cool post','created_at' => now(),'updated_at' => now()]
+        ]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+}
