bc18de13a4db5990be62a3b41873fecb83eb5f76
Ümit UZ <umituz998@gmail.com>
2020-05-24 14:18:07 +0300

    GetAuthUserTest file created

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function () {
+
+    Route::get('/auth-user','AuthUserController@show');
+
     Route::apiResources([
         '/posts' => 'PostController',
         '/users' => 'UserController',
